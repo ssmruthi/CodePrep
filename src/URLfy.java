53fa79f54b64bf08fcbb541dcfb255aee3495415
ssmruthi <smruthi.sridhar@gmail.com>
2017-05-22 14:18:38 -0700

    no message

--- src/URLfy.java
+++ src/URLfy.java
@@ -1,5 +1,6 @@
 public class URLfy {
 
+	//O(n) space and O(n) Time
 	public String replaceSpace(String input, int trueLen){
 			
 		char[] newString1 = new char[trueLen*3];
@@ -16,5 +17,26 @@ public class URLfy {
 		}
 		System.out.println(new String(newString1));
 		return newString1.toString();
+	}
+	
+	public static String replace20(String input, int truelen){
+		
+		char[] inp=input.toCharArray();
+		int start=truelen-1;
+		int end=inp.length-1;	
+		
+		while(start!=end){
+			
+			if(inp[start]==' '){
+				inp[end--]='0';
+				inp[end--]='2';
+				inp[end--]='%';
+				start--;
+			}else{
+				inp[end--]=inp[start--];
+			}
+		}
+		
+		return new String(inp);		
 	}
 }
--- src/UniqueString.java
+++ src/UniqueString.java
@@ -1,16 +1,39 @@
+import java.util.Arrays;
 
 public class UniqueString {
 
-	public boolean isUniqueString(String inputStr){
-	
-		//mergeSort(inputString);
+	//O(nlogn)
+	public static boolean isUniqueString(String inputStr){
 		
-		for(int i=1;i<inputStr.length();i++){
-			if(inputStr.charAt(i)==inputStr.charAt(i-1)){
-				System.out.println("cat");
+		if(inputStr.length()>128)
+			return false;
+		
+		char[] input= inputStr.toLowerCase().toCharArray();
+		Arrays.sort(input);
+		
+		for(int i=0;i<input.length-1;i++){
+			if(input[i]==input[i+1])
 				return false;
-			}
 		}
 		return true;
+	}
+	
+	public static boolean isUniqueBitVector(String input){
+		
+		int counter=0;
+		char[] inputStr= input.toLowerCase().toCharArray();
+		
+		for(char x:inputStr){
+			int val=x-'a';
+			if((counter & (1<<val)) >0) {			
+				
+				return false;
+			}
+			
+			counter|=1<<val;
+		}
+		
+		return true;
+		
 	}
 }
