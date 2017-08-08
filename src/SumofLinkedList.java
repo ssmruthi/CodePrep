e523cb5a69223bf1b8b6c732bc4cd4007d5aa0a2
Sridhar, Smruthi <smsridhar@paypal.com>
2017-08-08 15:04:11 -0700

    Sum of LinkedList (forward and backward)

--- src/SumofLinkedList.java
+++ src/SumofLinkedList.java
@@ -48,4 +48,79 @@ public class SumofLinkedList {
 	
 	}
 	
+	
+	/*
+	 *Sum of two linked list
+	 *Values stored forward 
+	 * Eg : 213 is represented as 2->1->3
+	 *      521 is represented as 5->2->1
+	 *      Sum 7>3>4 is 734
+	 */
+	public static Node sum2(Node h1, Node h2){
+		
+		int n1=0;
+		int n2=0;
+		
+		Node r=null;
+		
+		Node head1=h1;
+		Node head2=h2;
+		
+		while(head1!=null){
+			n1++;
+			head1=head1.next;
+		}
+		
+		while(head2!=null){
+			n2++;
+			head2=head2.next;
+		}
+		
+		while(n1<n2){
+			Node node=new Node(0);
+			node.next=h1;
+			h1=node;
+			n1++;
+		}
+		while(n2<n1){
+			Node node=new Node(0);
+			node.next=h2;
+			h2=node;
+			n2++;
+		}
+		
+		r=recurseSum(null,h1,h2,0);
+		return r;
+		
+	}
+
+
+
+	private static Node recurseSum(Node r,Node h1, Node h2,int carry) {
+
+	
+		if(h1==null && h2==null){
+			return null;
+		}
+			
+		r=recurseSum(r,h1.next,h2.next,carry);
+		int value=	h1.data+h2.data+carry;
+		carry=value>10?1:0;
+		
+		Node newNode= new Node(value%10);
+		newNode.next=r;
+		r=newNode;
+
+		return r;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
